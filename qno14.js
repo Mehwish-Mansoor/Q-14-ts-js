01913c8c9048e5944277258fb0029542f24c8d36
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-13 11:53:32 +0500

    Add files via upload

---
+++ qno14.js
@@ -0,0 +1,7 @@
+"use strict";
+// 14. Guest List:  If you could invite anyone, living or deceased,
+//  to dinner, who would you invite? Make  a list that include at 
+//  least three people you'like to invite to dinner. then use your 
+//  list to print a message to each person, inviting them to dinner. 
+let gustArr = ["Mehwish", "Mansoor", "fatima", "Umm-e-Hani"];
+gustArr.map((item) => console.log(` Dear ${item}, you are invited to a Dinner.`));
